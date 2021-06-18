c0fdfd86f8a7d24786d6ad82c30ad218faf31e84
TusherRakib <49785991+TusherRakib@users.noreply.github.com>
2021-06-18 19:33:05 +0600

    Tusher - Project Upload

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BloodSource());
+}
+
+class BloodSource extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
