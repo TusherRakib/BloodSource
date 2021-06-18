3c0b4df94e58193167fc3c14ec38e615e5267c82
RafidKhan <rafidkhan321.rk@gmail.com>
2021-06-18 23:45:25 +0600

    Rafid-login screen 2nd phase

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:blood_source/Screens/dashboard_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -11,21 +13,61 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding:
+            EdgeInsets.only(top: 35.0, bottom: 20.0, left: 10.0, right: 10.0),
         child: Column(
           children: [
-            Image.asset('assets/images/user_default.png'), //TODO image unloading
-            Image.asset('assets/images/user_profile_pic_default.jpg'), //TODO image unloading
-            Text('Hello'),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(
-                    builder:(_) => Dashboard(),
+            Container(
+                height: 280,
+                width: 200,
+                child: SvgPicture.asset(
+                  'assets/images/logo.svg',
+                  fit: BoxFit.cover,
+                )),
+            Text(
+              'Welcome Back',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Sign to continue',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  icon: Icon(
+                    Icons.mail,
+                    color: Colors.grey,
                   ),
+                  labelText: 'Email'),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  icon: Icon(
+                    Icons.lock,
+                    color: Colors.grey,
+                  ),
+                  labelText: 'Password'),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => Dashboard(),
+                    ),
                   );
                 },
-                child: Text('Log IN',style: TextStyle(fontSize: 20.0),)
-            )
+                child: Text(
+                  'Log IN',
+                  style: TextStyle(fontSize: 20.0),
+                ))
           ],
         ),
       ),
